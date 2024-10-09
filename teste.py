c7b7ded1a93e932d46b9b1563706fd566adac7da
portugolsi <jonadabe.santana@dcx.ufpb.br>
2024-10-09 15:49:28 -0300

    Fiz alguma alteraççao

--- teste.py
+++ teste.py
@@ -2,4 +2,4 @@ print("Olá, mundo")
 # git init | Inicializa um repositório
 # git status | verifica o status do repositório
 # git add . | Adiciona todos os arquivos da pasta no repositório
-
+# git commit -m "Eu criei um arquivo para teste"
