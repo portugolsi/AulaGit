07d1049976daa579924f46e030fb028f50e8626c
portugolsi <jonadabe.santana@dcx.ufpb.br>
2024-10-09 16:01:15 -0300

    Todos os comandos agora estão aqui

--- teste.py
+++ teste.py
@@ -3,3 +3,7 @@ print("Olá, mundo")
 # git status | verifica o status do repositório
 # git add . | Adiciona todos os arquivos da pasta no repositório
 # git commit -m "Eu criei um arquivo para teste"
+# git config --global user.name "portugolsi" | Configurando o nome do meu usuário do github
+# git config --global user.email "jonadabe.santana@dcx.ufpb.br"
+# git push -u origin <nome da branch> | envia os documentos que estiverem na branch mencionada
+# git clone | baixa o repositório para a sua máquina
